a5d56ae47b242d473509d1d84be6c22c45d85469
Li <liguohui01@baidu.com>
2018-07-13 11:17:45 +0800

    libs add antd

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
 	appname: '$_appname',
 	localStaticDomain: '//localhost:8000',
 	publicStaticDomain: '//gh.bdstatic.com',
-	libs: ['react', 'react-dom', 'history', 'react-router-dom','react-redux', 'redux'],// 需要额外打包的公共库
+	libs: ['react', 'react-dom', 'history', 'react-router-dom','react-redux', 'redux', 'antd'],// 需要额外打包的公共库
 	supportFileType: ['jsx','less']// 需要支持的文件格式
 }
